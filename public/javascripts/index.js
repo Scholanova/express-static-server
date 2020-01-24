702b5d9bc5ea26f010f4c1ca767c132fc3ef7bb4
nicolas gomes <Gomes.Nicolas@outlook.fr>
2020-01-24 10:55:33 +0100

    New Correction

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,11 +4,11 @@ btns.forEach(element => {
   element.addEventListener("click", function (event) {
     console.log("Btn click !");
     event.target.style.color = "black"
-    
-    if (event.target.previousElementSibling.tagName == "button")
+
+    if (event.target.previousElementSibling.tagName == "BUTTON")
       event.target.previousElementSibling.style.color = "red";
 
-    if (event.target.nextElementSibling.tagName == "button")
+    if (event.target.nextElementSibling.tagName == "BUTTON")
       event.target.nextElementSibling.style.color = "red";
 
     console.log(event.target.getBoundingClientRect());
