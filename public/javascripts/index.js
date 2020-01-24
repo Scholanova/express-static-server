125e76d542b2c9b5174426be9f8f5bccb2edb793
nicolas gomes <Gomes.Nicolas@outlook.fr>
2020-01-24 10:52:49 +0100

    New Correction

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,16 @@
 let btns = window.document.querySelectorAll('button');
 
 btns.forEach(element => {
-  element.addEventListener("click", function(event){
+  element.addEventListener("click", function (event) {
     console.log("Btn click !");
     event.target.style.color = "black"
-    event.target.previousElementSibling.style.color = "red";
-    event.target.nextElementSibling.style.color = "red";
+    
+    if (event.target.previousElementSibling.tagName == "button")
+      event.target.previousElementSibling.style.color = "red";
+
+    if (event.target.nextElementSibling.tagName == "button")
+      event.target.nextElementSibling.style.color = "red";
+
+    console.log(event.target.getBoundingClientRect());
   });
 });
