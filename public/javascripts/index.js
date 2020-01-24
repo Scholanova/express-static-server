44da9425e160962948a703a8d18a61e6458b5b5e
Antoine Boileau <antoine@boileau.io>
2020-01-24 16:06:55 +0100

    Ajout du compteur de téléchargement avec synchro multionglets

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,43 +1,63 @@
-let message = window.document.querySelector('#message');
-let listUL = window.document.querySelector('#listUL');
-let Loaded = true;
+let message = window.document.querySelector('#message')
+let listUL = window.document.querySelector('#listUL')
+let Loaded = true
+
+if (!window.localStorage.getItem('counter')) {
+  window.localStorage.setItem('counter', 0)
+}
+let counter = window.document.querySelector('#counter')
+counter.textContent = window.localStorage.getItem('counter')
+
+window.addEventListener('storage', function () {
+  // Lire les modifications du local storage qui ont lieu dans d'autres onglets
+  console.log('storage event !')
+  counter.textContent = window.localStorage.getItem('counter')
+})
 
 for (let i = 0; i < 2; i++) {
   fetch('/api/data')
     .then(function (response) {
-      return response.json();
+      return response.json()
     })
     .then(function (response) {
-      LoadingUL(response);
+      LoadingUL(response)
+      incrementLocalStorageCounter()
     })
-    .catch(error => console.log("Erreur : " + error));
+    .catch(error => console.log('Erreur : ' + error))
 }
 
-window.addEventListener("scroll", function () {
+window.addEventListener('scroll', function () {
   if (this.window.innerHeight > message.getBoundingClientRect().top && Loaded == true) {
-    message.textContent = "Loading ...";
-    console.log("Loading ...");
-    Loaded = false;
+    message.textContent = 'Loading ...'
+    console.log('Loading ...')
+    Loaded = false
 
     fetch('/api/data')
       .then(function (response) {
-        return response.json();
+        return response.json()
       })
       .then(function (response) {
-        console.log(response);
-        LoadingUL(response);
-        message.textContent = "Loaded";
-        Loaded = true;
-        console.log("Loaded");
+        console.log(response)
+        LoadingUL(response)
+        message.textContent = 'Loaded'
+        Loaded = true
+        incrementLocalStorageCounter()
+        console.log('Loaded')
       })
-      .catch(error => console.log("Erreur : " + error));
+      .catch(error => console.log('Erreur : ' + error))
   }
-});
+})
 
-function LoadingUL(response) {
+function LoadingUL (response) {
   response.data.forEach(element => {
-    let li = document.createElement('li');
-    li.innerHTML = element.text;
-    listUL.appendChild(li);
-  });
+    let li = document.createElement('li')
+    li.innerHTML = element.text
+    listUL.appendChild(li)
+  })
+}
+
+function incrementLocalStorageCounter () {
+  const initialCounter = window.localStorage.getItem('counter')
+  window.localStorage.setItem('counter', parseInt(initialCounter) + 1)
+  counter.textContent = window.localStorage.getItem('counter')
 }
