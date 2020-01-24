6feef69b1ce8e7d4b772b93eedae1676f89f3e86
nicolas gomes <Gomes.Nicolas@outlook.fr>
2020-01-24 15:05:18 +0100

    new

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,43 @@
-let btns = window.document.querySelectorAll('button');
+let message = window.document.querySelector('#message');
+let listUL = window.document.querySelector('#listUL');
+let Loaded = true;
 
-btns.forEach(element => {
-  element.addEventListener("click", function(event){
-    console.log("Btn click !");
-    event.target.style.color = "black"
-    event.target.previousElementSibling.style.color = "red";
-    event.target.nextElementSibling.style.color = "red";
-  });
+for (let i = 0; i < 2; i++) {
+  fetch('/api/data')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      LoadingUL(response);
+    })
+    .catch(error => console.log("Erreur : " + error));
+}
+
+window.addEventListener("scroll", function () {
+  if (this.window.innerHeight > message.getBoundingClientRect().top && Loaded == true) {
+    message.textContent = "Loading ...";
+    console.log("Loading ...");
+    Loaded = false;
+
+    fetch('/api/data')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        console.log(response);
+        LoadingUL(response);
+        message.textContent = "Loaded";
+        Loaded = true;
+        console.log("Loaded");
+      })
+      .catch(error => console.log("Erreur : " + error));
+  }
 });
+
+function LoadingUL(response) {
+  response.data.forEach(element => {
+    let li = document.createElement('li');
+    li.innerHTML = element.text;
+    listUL.appendChild(li);
+  });
+}
